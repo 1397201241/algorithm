e48ce4eb0fe6c6ebb9d65644cd0dffa9f93ab25e
okeyupppp <okeyupppp>
2023-05-10 15:07:09 +0800

    tip:  罗马转整数 2.0

--- 字符串/romanToInt.ts
+++ 字符串/romanToInt.ts
@@ -1,3 +1,25 @@
+/**
+ * 罗马数转整数
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 示例 3:
+ * 输入: s = "IX"
+ * 输出: 9
+ * 示例 4:
+ * 输入: s = "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ * 示例 5:
+ * 输入: s = "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+* */
 const romanToInt = function(s: string) {
     const arr = s.split("");
     const len = arr.length;
